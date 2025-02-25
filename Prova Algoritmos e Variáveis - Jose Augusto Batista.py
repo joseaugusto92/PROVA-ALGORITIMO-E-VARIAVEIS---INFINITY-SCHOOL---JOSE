29fcc6ccf68a3efc2fb47b6469992601aed279ba
Jose Augusto  Batista de Oliveira <gutto.olveira@gmail.com>
2025-02-25 15:11:59 -0300

    Add files via upload
    segue aqui o codigo que está no arquivo. 
    
    base = int(input("Olá Amigo, vamos calcular a área do seu RETANGULO! Favor informe o tamanho da BASE: "))
    altura = int(input("agora insira o tamanha da ALTURA: "))
    area = base*altura
    print("A sua ÁREA é: ", area)

---
+++ Prova Algoritmos e Variáveis - Jose Augusto Batista.py
@@ -0,0 +1,4 @@
+base = int(input("Olá Amigo, vamos calcular a área do seu RETANGULO! Favor informe o tamanho da BASE: "))
+altura = int(input("agora insira o tamanha da ALTURA: "))
+area = base*altura
+print("A sua ÁREA é: ", area)
